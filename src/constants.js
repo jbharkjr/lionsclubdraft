fbb1caee99570f794f51cef97c80176a128e72a8
jbharkjr <brianharkness@gmail.com>
2026-06-24 19:54:15 -0500

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const STORAGE_KEY = 'lions-club-draft-phase-2b';
+export const SUPABASE_TABLE = 'draft_app_state';
+export const SUPABASE_ROW_ID = 'main';
+export const COLORS = ['gold', 'blue', 'green', 'purple', 'red'];
